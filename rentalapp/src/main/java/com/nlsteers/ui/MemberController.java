368b0c437277734b2cb6f654932f32853d522615
Nathaniel Steers <nathaniel2.steers@live.uwe.ac.uk>
2017-02-14 17:30:25 +0000

    CRUD in place for Members, further to extended roll out for Transactions and Items. Work on relational databases to follow.

--- rentalapp/src/main/java/com/nlsteers/ui/MemberController.java
+++ rentalapp/src/main/java/com/nlsteers/ui/MemberController.java
@@ -12,6 +12,8 @@ import com.nlsteers.ui.member.SearchMembers;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -37,6 +39,16 @@ public class MemberController {
 
     public void save() {
         memberDAO.merge(editMembers.getMember());
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getFlash().setKeepMessages(true);
+                context.addMessage(null, new FacesMessage("Successfully created new member " + editMembers.getMember().getFirstName() + " " + editMembers.getMember().getLastName()));
+    }
+
+
+    public void remove(Member mem) {
+        memberDAO.remove(mem);
+        FacesContext.getCurrentInstance()
+                .addMessage(null, new FacesMessage("Successfully deleted member " + mem.getFirstName() + " " + mem.getLastName()));
     }
 
     public void preRenderViewEvent() {
